d6f5391c8ba8a56dc3344f835d0145edd928be5c
dineshgiri <dineshgiri1542@gmail.com>
2023-06-11 19:10:33 +0530

    add functional component

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", {}, "hello  giri🙋");
 
-console.log("heading", heading); // thiis is rerutn object not heading
+// react element
+
+// jsx (transpile befor it reaches the js engine) -parce -Bable
+
+// these is cor
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hey i am ract 🙋"
+// );
+
+// // return object
+// console.log("heading", heading);
+
+// react element
+const jsxHeading = (<h1 id="heading">"namse react bhai using jsx🚀"</h1>);
+
+// react component
+
+const Greeting =()=>  <h1>"hello giri🙋"</h1>
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log("root", root);
-root.render(heading); // the render method basically convert to object in heading;
+root.render(<Greeting/>);
